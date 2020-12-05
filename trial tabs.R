36fedf4ea9aa356727fc42b0c45f71377113fe7a
Matthew Courtney <matthew.courtney@nu.edu.kz>
2020-12-05 11:51:39 +0600

    enough details now?

--- trial tabs.R
+++ trial tabs.R
@@ -1,6 +1,6 @@
 # Kevin to give review of version number below:
 # it's like a global variable that makes changes throughout trail tabs
-# We can focus on this later!
+# We can focus on this later! more details to come!
 
 version_number <- "0.1.0"
 
